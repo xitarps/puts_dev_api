ffe769f0c3166e0ed134f7f692f86c5a5c3fa104
XitaRps <xitarps@gmail.com>
2024-07-09 05:16:20 -0300

    Feat: Add articles model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+articles = [
+  {title: 'art 1', content: 'conteudo do art 1'},
+  {title: 'art 2', content: 'conteudo do art 2'},
+  {title: 'art 3', content: 'conteudo do art 3'}
+]
+
+Article.create(articles)
